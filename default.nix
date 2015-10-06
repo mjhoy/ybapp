52280479bca02d97697911ee7ccd90396df97b2c
Michael Hoy <mjh@mjhoy.com>
2015-10-06 13:13:48 -0500

    update nix deps

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, stdenv, xlsx }:
+{ mkDerivation, base, bytestring, containers, lens, stdenv, text
+, xlsx
+}:
 mkDerivation {
   pname = "ybapp";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base xlsx ];
+  executableHaskellDepends = [
+    base bytestring containers lens text xlsx
+  ];
   description = "Downloader for Yellow Barn applicant files";
   license = stdenv.lib.licenses.mit;
 }
--- shell.nix
+++ shell.nix
@@ -4,14 +4,18 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv, xlsx }:
+  f = { mkDerivation, base, bytestring, containers, lens, stdenv
+      , text, xlsx
+      }:
       mkDerivation {
         pname = "ybapp";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
-        executableHaskellDepends = [ base xlsx ];
+        executableHaskellDepends = [
+          base bytestring containers lens text xlsx
+        ];
         description = "Downloader for Yellow Barn applicant files";
         license = stdenv.lib.licenses.mit;
       };
